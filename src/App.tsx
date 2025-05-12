70c36c0aa7a5410d627f71377804c8993643a887
akihiro.OKAYAMA <akihiro.okayama@sencorp.co.jp>
2025-05-12 20:14:31 +0900

    feat: tailwindを追加した

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import type { Component } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
+import './inde.css';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
+        <h1 class="text-3xl font-bold underline bg-red-500">
+          Hello world!
+        </h1>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
